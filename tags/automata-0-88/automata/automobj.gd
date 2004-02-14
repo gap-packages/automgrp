46885fed31dd099364d0dbdae4782684a03e4cc4
nobody <nobody@ac7e4ea9-986b-4c28-91e1-da6ada7c0edf>
2004-02-14 23:59:24 +0000

    This commit was manufactured by cvs2svn to create tag 'automata-0-88'.

---
+++ tags/automata-0-88/automata/automobj.gd
@@ -0,0 +1,43 @@
+#############################################################################
+##
+#W  automobj.gd              automata package                  Yevgen Muntyan
+##
+##  automata v0.9, started 01/22/2004
+##
+
+
+##
+##	AutomObj is category parent for all automata categories - IsAutom, 
+##	IsAGroup etc.
+##
+
+###############################################################################
+##
+#C  IsAutomObj
+##
+DeclareCategory("IsAutomObj", IsObject);
+
+
+###############################################################################
+##
+#O  Projection(<object>, <number>)
+#O  LeftProjection(<object>)
+#O  RightProjection(<object>)
+##
+DeclareOperation("Projection", [IsAutomObj, IsInt]);
+DeclareOperation("LeftProjection", [IsAutomObj]);
+DeclareOperation("RightProjection", [IsAutomObj]);
+
+
+###############################################################################
+##
+#O  Perm(<autom>, <level>)
+#A  TopPerm(<autom>)
+##
+DeclareOperation("Perm", [IsAutomObj, IsInt]);
+DeclareAttribute("TopPerm", IsAutomObj);
+
+
+
+
+

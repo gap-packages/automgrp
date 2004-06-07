c5975fbec94fd3039ba917464731a080a1f79603
emuntyan <emuntyan@ac7e4ea9-986b-4c28-91e1-da6ada7c0edf>
2004-06-07 14:38:16 +0000

    autom.g* merged

---
+++ trunk/automata/autom.gd
@@ -0,0 +1,49 @@
+#############################################################################
+##
+#W  autom.gd                   automata package                Yevgen Muntyan
+#W                                                             Dmytro Savchuk
+##  automata v 0.91 started June 07 2004
+##
+
+Revision.autom_gd := 
+  "@(#)$Id$";
+
+
+###############################################################################
+##
+#C  IsAutom
+##
+DeclareCategory("IsAutom", IsInitialAutomaton);
+DeclareCategoryCollections("IsAutom");
+
+
+#############################################################################
+##
+#C  IsAutomFamily
+##
+DeclareCategoryFamily( "IsAutom" );
+
+
+###############################################################################
+##
+#O  CreateAutom(<object>)
+##
+DeclareOperation("CreateAutom", [IsObject]);
+
+
+###############################################################################
+##
+#O  Autom(<object>)
+##
+DeclareOperation("Autom", [IsObject]);
+
+
+###############################################################################
+##
+#P  IsActingOnBinaryTree
+##
+DeclareProperty("IsActingOnBinaryTree", IsAutomFamily);
+
+
+
+#E

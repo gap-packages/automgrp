6a04659cd7a34d5c72d1c649e0df4b9eb9fc9224
emuntyan <emuntyan@ac7e4ea9-986b-4c28-91e1-da6ada7c0edf>
2004-06-07 12:24:36 +0000

    IsCorrectAutomatonList is rewritten.

--- trunk/automata/listops.gd
+++ trunk/automata/listops.gd
@@ -12,85 +12,86 @@ Revision.listops_gd :=
 
 ###############################################################################
 ##
-#F  IsCorrectListCompOfAutomaton( list )
+#F  IsCorrectAutomatonList( <list> )
 ##
-##  checks if given list can be list component of automaton structure
-##  returns list with two components: first component is true/false,
-##  second one is error message
+##  Checks whether the list is the following:
+##  [[a_11,...,a_1n,p_1],[a_21,...,a_2n,p_2],...,[a_m1...a_mn,p_m]],
+##  where n >= 2, m >= 1, a_ij are IsInt in [1..m], p_i are IsPerm, and 
+##  maximal moved point of each p_i is not greater than n.
 ##
-DeclareGlobalFunction("IsCorrectListComponentOfAutomaton");
+DeclareGlobalFunction("IsCorrectAutomatonList");
 
 
-###############################################################################
-##
-#F  ConnectedStatesInList(state, list)
-##
-##  Returns list of states which are reachable from given state,
-##  it does not check correctness of arguments
-##
-DeclareGlobalFunction("ConnectedStatesInList");
-
-
-###############################################################################
-##
-#F  IsTrivialStateInList(state, list)
-##
-##  it does not check correctness of arguments
-##
-DeclareGlobalFunction("IsTrivialStateInList");
-
-
-###############################################################################
-##
-#F  AreEquivalentStatesInList(state1, state2, list)
-##
-##  it does not check correctness of arguments
-##
-DeclareGlobalFunction("AreEquivalentStatesInList");
-
-
-###############################################################################
-##
-#F  AreEquivalentStatesInLists(state1, state2, list1, list2)
-##
-##  it does not check correctness of arguments
-##
-DeclareGlobalFunction("AreEquivalentStatesInLists");
-
-
-###############################################################################
-##
-#F  ReducedAutomatonInList(list)
-##
-##  returns new list which is list representation of reduced form of automaton
-##  given by list
-##  first state of returned list is always first state if given one
-##  this function does not remove trivial state - it's for initial automata
-##
-##  It does not check correctness of list
-##
-DeclareGlobalFunction("ReducedAutomatonInList");
-
-
-###############################################################################
-##
-#F  MinimalSubAutomatonInlist(<states>, <list>)
-##
-##  Returns list rep of automaton which is minimal subatomaton of automaton
-##  with list rep <list> containing states <states>.
-##
-##  It does not check correctness of list
-##
-DeclareGlobalFunction("MinimalSubAutomatonInlist");
-
-
-###############################################################################
-##
-#F  PermuteStatesInList(<list>, <perm>)
-##
-##  It does not check correctness of arguments
-##
-DeclareGlobalFunction("PermuteStatesInList");
+# ###############################################################################
+# ##
+# #F  ConnectedStatesInList(state, list)
+# ##
+# ##  Returns list of states which are reachable from given state,
+# ##  it does not check correctness of arguments
+# ##
+# DeclareGlobalFunction("ConnectedStatesInList");
+# 
+# 
+# ###############################################################################
+# ##
+# #F  IsTrivialStateInList(state, list)
+# ##
+# ##  it does not check correctness of arguments
+# ##
+# DeclareGlobalFunction("IsTrivialStateInList");
+# 
+# 
+# ###############################################################################
+# ##
+# #F  AreEquivalentStatesInList(state1, state2, list)
+# ##
+# ##  it does not check correctness of arguments
+# ##
+# DeclareGlobalFunction("AreEquivalentStatesInList");
+# 
+# 
+# ###############################################################################
+# ##
+# #F  AreEquivalentStatesInLists(state1, state2, list1, list2)
+# ##
+# ##  it does not check correctness of arguments
+# ##
+# DeclareGlobalFunction("AreEquivalentStatesInLists");
+# 
+# 
+# ###############################################################################
+# ##
+# #F  ReducedAutomatonInList(list)
+# ##
+# ##  returns new list which is list representation of reduced form of automaton
+# ##  given by list
+# ##  first state of returned list is always first state if given one
+# ##  this function does not remove trivial state - it's for initial automata
+# ##
+# ##  It does not check correctness of list
+# ##
+# DeclareGlobalFunction("ReducedAutomatonInList");
+# 
+# 
+# ###############################################################################
+# ##
+# #F  MinimalSubAutomatonInlist(<states>, <list>)
+# ##
+# ##  Returns list rep of automaton which is minimal subatomaton of automaton
+# ##  with list rep <list> containing states <states>.
+# ##
+# ##  It does not check correctness of list
+# ##
+# DeclareGlobalFunction("MinimalSubAutomatonInlist");
+# 
+# 
+# ###############################################################################
+# ##
+# #F  PermuteStatesInList(<list>, <perm>)
+# ##
+# ##  It does not check correctness of arguments
+# ##
+# DeclareGlobalFunction("PermuteStatesInList");
 
 
 
